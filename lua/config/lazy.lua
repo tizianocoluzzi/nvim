d26f6d8c678546b8b264d1f242ca43ca2cbb908c
tizianocoluzzi <coluzzi.tizi@gmail.com>
2024-12-29 11:32:58 +0100

    successfully changed default nvim shell to bash

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ local opts = { noremap = true, silent = true }
 map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
 
-
+vim.opt.shell = '/usr/bin/bash'
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,6 +4,7 @@ function _G.set_terminal_keymaps()
     local opts = {noremap = false}
     vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+    vim.keymap.set('t', '<Up>', [[<up>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
