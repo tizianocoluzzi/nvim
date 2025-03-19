0d6f5c5ffb0c5a9bc8d5f6408177889ea914efca
tiziano <coluzzi.tizi@gmail.com>
2025-03-19 14:00:56 +0100

    numeri relativi

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,7 @@ vim.opt.shell = '/usr/bin/zsh'
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.wo.number = true
+vim.wo.relativenumber = true
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
